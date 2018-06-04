4aa25e0873ea489b807de9556414434641f03139
Jonathan <jtecktips@gmail.com>
2018-06-04 16:12:37 -0700

    Recipe Detail Creation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'recipe-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [{
+    name: 'A new recipe',
+    description: 'test recipe',
+    imagePath: 'https://d2gk7xgygi98cy.cloudfront.net/6-3-large.jpg'
+  },
+  new Recipe('A Test Recipe',
+  'This is a simple test.',
+  'https://www.tablespoon.com/-/media/Images/CollectionHeroes/16/05/summer-pinterest_HERO_680x384.jpg?W=800')
+];
 
   constructor() { }
 
